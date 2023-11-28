9564b7ce256e7a5b5fb37c74c8fa88806e3e3b10
Brunolops <[200~brunooliveira99874@hotmail.com>
2023-11-27 22:18:42 -0300

    criação de classes

---
+++ src/model/entities/Salary.java
@@ -0,0 +1,17 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Salary {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private baseSalary Double;
+	
+	private Department department;
+	
+	
+
+}
