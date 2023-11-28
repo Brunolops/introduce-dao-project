e4132ea305671d8df20887d33dacc3e8d314f411
Brunolops <[200~brunooliveira99874@hotmail.com>
2023-11-28 11:30:02 -0300

    findAll implemantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,8 @@ public class Program {
 		Department dep = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
 		
+		System.out.println("\n=== TEST 3: seller findAll ====");
+		 list = sellerDao.findall();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
