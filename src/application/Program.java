e2df06e68d9653948db87f8ca2ddd5d698da37e3
Brunolops <[200~brunooliveira99874@hotmail.com>
2023-11-28 15:03:37 -0300

    delete implemantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,12 +26,18 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== TEST 3: seller findAll ====");
+		System.out.println("\n=== TEST 4: seller findAll ====");
 		
 		Seller newSeller = new Seller(null, "Greg", "greg@hotmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
 		
 		System.out.println("done!" + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Update Seller ====");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Purple");
+		sellerDao.update(seller);
 		
 		
 	}
